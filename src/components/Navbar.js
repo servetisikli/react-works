4c173fe3e05c65b70d9179b618b0bc5b60749808
servetisikli <servetisikli@gmail.com>
2023-06-05 23:30:14 +0200

    appBar color is changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ export default function Navbar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" style={{ background: "#046582" }}>
         <Toolbar>
           <IconButton
             size="large"
